1cb462eeca4f94a2a43596aaa0e015f9abda800a
usamatcheema <usamatcheema@gmail.com>
2019-12-26 02:49:20 -0500

    adding tags

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,19 +2,38 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 2,
+    tags: []
   };
+
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags.</p>;
+
+    return (
+      <ul>
+        {this.state.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
   render() {
     return (
       <React.Fragment>
-        <span>{this.formatCount}</span>
-        <button>increment</button>
+        {this.state.tags.length === 0 && "Please create a new tag"}
+        {this.renderTags()}
       </React.Fragment>
     );
   }
 
+  getBadgeClasses() {
+    let classes = "badge m-4 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
-    const { count } = this.state.count;
+    const { count } = this.state;
     return count === 0 ? "Zero" : count;
   }
 }
