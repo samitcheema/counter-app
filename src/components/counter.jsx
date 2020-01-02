a4b9a59b032e49a7fa24f93c5114f89ccdc7a0b9
usamatcheema <usamatcheema@gmail.com>
2020-01-02 02:47:08 -0500

    deleting counters

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,11 +2,11 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    value: this.props.counter.value
   };
 
   handleIncrement = () => {
-    this.setState({ count: this.state.count + 1 });
+    this.setState({ value: this.state.value + 1 });
   };
 
   doHandleIncrement = () => {
@@ -24,6 +24,12 @@ class Counter extends Component {
           >
             increment
           </button>
+          <button
+            onClick={() => this.props.onDelete(this.props.counter.id)}
+            className="btn btn-danger btn-sm m-2"
+          >
+            Delete
+          </button>
         </div>
       </React.Fragment>
     );
@@ -31,13 +37,13 @@ class Counter extends Component {
 
   getBadgeClasses() {
     let classes = "badge m-4 badge-";
-    classes += this.state.count === 0 ? "warning" : "primary";
+    classes += this.state.value === 0 ? "warning" : "primary";
     return classes;
   }
 
   formatCount() {
-    const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    const { value } = this.state;
+    return value === 0 ? "Zero" : value;
   }
 }
 
